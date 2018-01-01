f291ac9518c319775c30dfd935c07c239ab1ee62
Angeline <atan009@gmail.com>
2018-01-01 20:59:57 +0800

    Modify wait_for_row_in_table to implicitly wait for a max of 10 seconds when checking for rows
    - to avoid assertion error when the page has not loaded

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,13 @@
 import time
+
+from django.test import LiveServerTestCase
 from selenium import webdriver
-from unittest import TestCase
+from selenium.common.exceptions import WebDriverException
 
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -13,18 +15,25 @@ class NewVisitorTest(TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == row_text for row in rows),
-            'New to-do item {} did not appear in list: {}'.format(row_text, [row.text for row in rows])
-        )
+    def wait_for_row_in_table(self, row_text):
+        MAX_WAIT = 10
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -38,6 +47,7 @@ class NewVisitorTest(TestCase):
             'Enter a to-do item'
         )
 
+
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
         input_box.send_keys('Buy peacock feathers')
@@ -47,7 +57,7 @@ class NewVisitorTest(TestCase):
         input_box.send_keys(Keys.ENTER)
         time.sleep(5)
 
-        self.check_for_row_in_table('1: Buy peacock feathers')
+        self.wait_for_row_in_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -57,8 +67,8 @@ class NewVisitorTest(TestCase):
         time.sleep(5)
 
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_table('2: Use peacock feathers to make a fly')
-        self.check_for_row_in_table('1: Buy peacock feathers')
+        self.wait_for_row_in_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_table('1: Buy peacock feathers')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
